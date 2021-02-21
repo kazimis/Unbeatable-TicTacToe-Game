8a2cb5c1be90f008369ff2833473dc9dd8288a57
sarajuddin <sarajkazimi@gmail.com>
2021-02-20 18:05:04 -0800

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -12,24 +12,33 @@ function App() {
     fetchData();
   }, []);
   console.log(data);
+  const playWitComp = (e) => {
+    document.getElementsByClassName("gameOption").style.display = "none";
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="gameOption col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+        <div className="card align-items-center">
+          <div className="card-body text-center">
+            <h1 class="Game option">Game option</h1>
+            <p class="card-text">
+              Choose to play with computer or other available player
+            </p>
+            <button
+              type="button"
+              class="btn btn-primary btn-lg btn-block mt-5"
+              onClick={playWitComp}
+            >
+              Play with computer
+            </button>
+            <button type="button" class="btn btn-primary btn-lg btn-block">
+              Play with other player
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
